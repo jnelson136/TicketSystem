6b59266ca1ffc5f6ef45c6c061e0d1eaaf7d851e
Joe Nelson <jnelson136@my.wctc.edu>
2024-03-13 14:41:38 -0500

    Add ReadTicket method

--- TicketHandle.cs
+++ TicketHandle.cs
@@ -2,10 +2,36 @@ using System;
 using System.Collections.Generic;
 using System.IO;
 
-public class TicketHandle {
+public class TicketHandle
+{
     private string filePath;
 
-    public TicketHandle(string filePath) {
+    public TicketHandle(string filePath)
+    {
         this.filePath = filePath;
+    }
+
+    public void ReadTicket()
+    {
+        StreamReader sr = new StreamReader(filePath);
+        try
+        {
+            if (File.Exists(filePath))
+            {
+                while (!sr.EndOfStream)
+                {
+                    string line = sr.ReadLine();
+                    Console.WriteLine(line);
+                }
+            }
+            else
+            {
+                Console.WriteLine("File Does Not Exist");
+            }
+        }
+        finally
+        {
+            sr.Close();
+        }
     }
 }
