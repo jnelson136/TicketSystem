89810c5e5f5bf58820cdecb7c9bf85f7d5afe836
Joe Nelson <jnelson136@my.wctc.edu>
2024-03-13 14:27:52 -0500

    Add Constructor to TicketHandle.cs

---
+++ TicketHandle.cs
@@ -0,0 +1,11 @@
+using System;
+using System.Collections.Generic;
+using System.IO;
+
+public class TicketHandle {
+    private string filePath;
+
+    public TicketHandle(string filePath) {
+        this.filePath = filePath;
+    }
+}
